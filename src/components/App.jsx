a60114c60458626800d314f7cc54fe3debdb27ce
bapjiws <alex.boklin@gmail.com>
2017-07-15 01:01:09 +0300

    Implement Reviews component that renders one review w/o images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const App = () => <h1>Hey there!</h1>;
+import Reviews from './Reviews';
+
+const App = () => <div>
+    <h1 style={{textAlign: 'center'}}>Guess the Rating!</h1>
+    <Reviews />
+</div>;
 
 export default App;
