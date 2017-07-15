70b7604b922992993eb939468346dc8074c0fc38
bapjiws <alex.boklin@gmail.com>
2017-07-15 14:52:21 +0300

    Fade in reviews one by one

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -7,6 +7,20 @@ const ratings = importAll(require.context('../../assets/images/stars', false, /\
 const reviewerPortraits = importAll(require.context('../../assets/images/reviewers', false, /\.(jpe?g|png|gif|svg)$/));
 
 export default class Reviews extends Component {
+    componentDidMount() {
+        const reviews = document.querySelectorAll("div[class='review-container']");
+        // console.log('reviews:', reviews);
+        const reviewsLength = reviews.length;
+        for (let i = 0; i <= reviewsLength-1; i++) {
+            // console.log('review:', reviews[i]);
+            (index => {
+                setTimeout(function () {
+                    // console.log('SHOWING:', reviews[index] );
+                    reviews[index].style.opacity = 1;
+                }, 100 * (index+1)); // TODO: set to 500 when finished debugging
+            })(i);
+        }
+    }
 
     render() {
         return <div className="reviews-container">
