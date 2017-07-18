4ab7a2b962fa3fa61404b92ec9ec6f2c8c0cc330
bapjiws <alex.boklin@gmail.com>
2017-07-18 09:36:55 +0300

    TypeScript setup for tests

--- __tests__/findStarByMousePosX.js
+++ __tests__/findStarByMousePosX.js
@@ -1,37 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 require('jasmine-check').install();
-import findStarByMousePosX from '../utils/findStarByMousePosX';
-
-const width = 260;
-
-const stars = [1, 2, 3, 4, 5].reduce(function (acc, curr, _, array) {
+var findStarByMousePosX_1 = require("../utils/findStarByMousePosX");
+var width = 260;
+var stars = [1, 2, 3, 4, 5].reduce(function (acc, curr, _, array) {
     acc.push({
         rating: curr,
         domRef: '.rating-' + curr,
-        left: width / array.length * (curr - 1), // left boundary included
-        right: curr ===  array.length ? width / array.length * curr: width / array.length * curr - 1 // right boundary excluded (except the very last interval)
+        left: width / array.length * (curr - 1),
+        right: curr === array.length ? width / array.length * curr : width / array.length * curr - 1 // right boundary excluded (except the very last interval)
     });
     return acc;
 }, []);
-
-describe('findStarByMousePosX', () => {
-    check.it(
-        'should not return undefined',
-        gen.posInt.suchThat(value => value <= width), {times: 1000},
-        mousePosX => expect(findStarByMousePosX(mousePosX, stars)).not.toBeUndefined()
-    );
-
-    check.it(
-        'should find something given that the input is within the width',
-        gen.posInt.suchThat(value => value <= width), {times: 1000},
-        mousePosX => expect(findStarByMousePosX(mousePosX, stars)).not.toBe(-1)
-    );
-
-    check.it(
-        'should correctly find the right interval',
-        gen.posInt.suchThat(value => value <= width), {times: 1000},
-        mousePosX => expect(
-            mousePosX >= stars[findStarByMousePosX(mousePosX, stars)].left &&
-                mousePosX <= stars[findStarByMousePosX(mousePosX, stars)].right
-        ).toBe(true)
-    );
+describe('findStarByMousePosX', function () {
+    check.it('should not return undefined', gen.posInt.suchThat(function (value) { return value <= width; }), { times: 1000 }, function (mousePosX) { return expect(findStarByMousePosX_1.default(mousePosX, stars)).not.toBeUndefined(); });
+    check.it('should find something given that the input is within the width', gen.posInt.suchThat(function (value) { return value <= width; }), { times: 1000 }, function (mousePosX) { return expect(findStarByMousePosX_1.default(mousePosX, stars)).not.toBe(-1); });
+    check.it('should correctly find the right interval', gen.posInt.suchThat(function (value) { return value <= width; }), { times: 1000 }, function (mousePosX) { return expect(mousePosX >= stars[findStarByMousePosX_1.default(mousePosX, stars)].left &&
+        mousePosX <= stars[findStarByMousePosX_1.default(mousePosX, stars)].right).toBe(true); });
 });
